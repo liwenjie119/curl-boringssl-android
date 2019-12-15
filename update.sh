537c6e4087163a5c36fc8e62cca2ccb16e2a4025
liwenjie119 <liwenjie119@126.com>
2019-12-15 12:54:08 +0800

    add my files

---
+++ update.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+git pull
+cd boringssl
+git checkout master
+git pull
+cd ../c-ares
+git checkout master
+git pull
+cd ../curl
+git checkout master
+git pull
+cd ../libexpat
+git checkout master
+git pull
+cd ../libmetalink
+git checkout master
+git pull
+cd ../libpsl
+git checkout master
+git pull
+cd ../libssh2
+git checkout master
+git pull
+cd ../nghttp2
+git checkout master
+git pull
+cd ../quiche
+git checkout master
+git pull
+cd ../zlib
+git checkout develop
+git pull
+cd ..
+
