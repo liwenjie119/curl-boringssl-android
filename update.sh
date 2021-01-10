0de0c752d94fceeeb65d952709f57cc382abcfa2
liwenjie119 <liwenjie119@126.com>
2021-01-10 23:53:27 +0800

    fix checkout

--- update.sh
+++ update.sh
@@ -2,39 +2,51 @@
 git pull
 cd boringssl
 git checkout master
+git checkout .
 git pull
 cd ../brotli
 git checkout master
+git checkout .
 git pull
 cd ../busybox
 git checkout master
+git checkout .
 git pull
 cd ../c-ares
 git checkout master
+git checkout .
 git pull
 cd ../curl
 git checkout master
+git checkout .
 git pull
 cd ../libexpat
 git checkout master
+git checkout .
 git pull
 cd ../libmetalink
 git checkout master
+git checkout .
 git pull
 cd ../libpsl
 git checkout master
+git checkout .
 git pull
 cd ../libssh2
 git checkout master
+git checkout .
 git pull
 cd ../nghttp2
 git checkout master
+git checkout .
 git pull
 cd ../openssl
 git checkout master
+git checkout .
 git pull
 cd ../zlib
 git checkout develop
+git checkout .
 git pull
 cd ..
 
