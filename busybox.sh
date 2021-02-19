8eada3215f86bcf0c13edf7aff99c34ffbe8bd97
liwenjie119 <liwenjie119@126.com>
2021-02-19 19:52:42 +0800

    patch-for-resolving-domaain-name

--- busybox.sh
+++ busybox.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 cp -f .config busybox/.config
+cp -f 0001-patch-for-resolving-domaain-name.patch busybox/0001-patch-for-resolving-domaain-name.patch 
 [ -f "gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz" ] || wget -q http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 [ -d "gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu" ] || tar -xJf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 #wget -q http://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/aarch64-linux-gnu/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 tar -xJf gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu.tar.xz
 export CROSS_COMPILE=$PWD/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-
 cd busybox
+git am 0001-patch-for-resolving-domaain-name.patch
 #自动保存并退出菜单
 
 #(echo -e \'\\0x65\'; echo -e \'\\0x79\') | make ARCH=arm64 menuconfig
